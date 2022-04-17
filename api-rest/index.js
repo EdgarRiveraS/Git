013ae257a2fc9ab0ddb8e3c1cffd3a9a5cafcd01
edgar <launchx08831@innovaccion.mx>
2022-04-16 22:31:56 -0500

    Se agregan los comandos get y post

--- api-rest/index.js
+++ api-rest/index.js
@@ -4,11 +4,29 @@ const bodyParser = require('body-parser');
 
 const app = express()
 const port = process.env.PORT || 3005
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hola/:name', (req, res) => {
-    res.send({ message: `Hola ${req.params.name}!` })
+app.get('/api/product', (req, res) => {
+    res.send(200,{products:[]})
+})
+
+app.get('/api/product/:productId',(req,res) => {
+
+})
+
+app.post('api/product', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: 'El producto se ha recibido'})
+})
+
+app.put('api/product/:productId', (req, res) => {
+    
+})
+
+app.delete('api/product/:productId', (req, res) => {
+    
 })
 
 app.listen(port, () => {
