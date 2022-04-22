218b299724040db399a3ded8ffc6e4238b3f0216
Edgar Rivera <launchx08831@innovaccion.mx>
2022-04-21 20:56:39 -0500

    Sin cambios reales

--- api-rest/index.js
+++ api-rest/index.js
@@ -7,7 +7,7 @@ const Product = require('./models/product')
 
 
 const app = express()
-const port = process.env.PORT || 3005
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
--- api-rest/models/product.js
+++ api-rest/models/product.js
@@ -1,6 +1,6 @@
 'use strict'
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const ProductSchema = Schema({
     name: String,
